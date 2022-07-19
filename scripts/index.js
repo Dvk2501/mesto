b59d9f819f9c0996e26439e63d95eecf81e1c61d
Dmitry Kravchenko <dmitry.kravchenko96@yandex.ru>
2022-07-19 17:08:59 +0300

    3

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
-const openButton = document.querySelector(".profile__button");
-const closeButton = document.querySelector(".popup__close");
-const popupNode = document.querySelector(".popup");
-const profileName = document.querySelector(".profile__title");
-const profileJob = document.querySelector(".profile__subtitle");
-const formElement = document.querySelector(".popup__form");
-const nameInput = formElement.querySelector(".form__input_type_name");
-const jobInput = formElement.querySelector(".form__input_type_job");
+const openButton = document.querySelector('.profile__button');
+const closeButton = document.querySelector('.popup__close');
+const popupNode = document.querySelector('.popup');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.form__input_type_name');
+const jobInput = formElement.querySelector('.form__input_type_job');
 
 function openPopup(modal) {
-  modal.classList.add("popup_opened");
+  modal.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
 function closePopup(modal) {
-  modal.classList.remove("popup_opened");
+  modal.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
@@ -24,12 +24,12 @@ function formSubmitHandler(evt) {
   closePopup(popupNode);
 }
 
-openButton.addEventListener("click", function () {
+openButton.addEventListener('click', function () {
   openPopup(popupNode);
 });
 
-closeButton.addEventListener("click", function () {
+closeButton.addEventListener('click', function () {
   closePopup(popupNode);
 });
 
-formElement.addEventListener("submit", formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
