9d4c4c26b40a62c4fec7a8e2f2dd7288af504377
Dmitry Kravchenko <dmitry.kravchenko96@yandex.ru>
2022-07-19 11:36:41 +0300

    3

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,35 @@
-const openButton = document.querySelector ('.profile__button');
-const closeButton= document.querySelector ('.popup__close');
-const popupNode = document.querySelector ('.popup');
-const profileName = document.querySelector ('.profile__title');
-const profileJob = document.querySelector ('.profile__subtitle');
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.form__input_name');
-const jobInput = formElement.querySelector ('.form__input_job');
+const openButton = document.querySelector(".profile__button");
+const closeButton = document.querySelector(".popup__close");
+const popupNode = document.querySelector(".popup");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__subtitle");
+const formElement = document.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".form__input_name");
+const jobInput = formElement.querySelector(".form__input_job");
 
-function openPopup (modal) {
-modal.classList.add('popup_opened')
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
+function openPopup(modal) {
+  modal.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closePopup(modal) {
-  modal.classList.remove('popup_opened')
+  modal.classList.remove("popup_opened");
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-profileJob.textContent = jobInput.value;
-   closePopup(popupNode)
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupNode);
 }
 
+openButton.addEventListener("click", function () {
+  openPopup(popupNode);
+});
 
-openButton.addEventListener ('click', function() {
-  openPopup(popupNode)
-})
+closeButton.addEventListener("click", function () {
+  closePopup(popupNode);
+});
 
-closeButton.addEventListener('click', function() {
-  closePopup(popupNode)
-})
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-
-
+formElement.addEventListener("submit", formSubmitHandler);
